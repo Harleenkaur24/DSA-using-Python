0fc88eba19d0b10e6c2d68ea9cfc14af3202610d
Harleen Kaur <138618300+Harleenkaur24@users.noreply.github.com>
2025-08-05 17:15:14 +0530

    Update traversal.py

--- Arrays/traversal.py
+++ Arrays/traversal.py
@@ -1,16 +1,6 @@
-def delete_index(self,index):
-        if self.head==None:
-            return 'Empty list'
-        if index==0:
-            return self.delete_head()
-        pos=0
-        temp=self.head
+def __str__(self):
+        result=''
+        for i in range(self.n):
+                result=result + str(self.A[i]) + ','
+        return '[' + result[:-1] + ']'
 
-        while(temp.next!=None and pos<index-1):
-            pos=pos+1
-            temp=temp.next
-            
-        if temp.next==None:
-           return 'Not found'
-        temp.next=temp.next.next
-        self.n =self.n -1 
