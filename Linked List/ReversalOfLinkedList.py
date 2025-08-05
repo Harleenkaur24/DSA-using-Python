028f075db7e83716383522541c6f028a4b67156b
Harleen Kaur <138618300+Harleenkaur24@users.noreply.github.com>
2025-08-05 16:56:36 +0530

    Create ReversalOfLinkedList.py

---
+++ Linked List/ReversalOfLinkedList.py
@@ -0,0 +1,11 @@
+def reverse(self):
+        prev_node=None
+        curr_node=self.head
+        while(curr_node!=None):
+            next_node=curr_node.next
+            curr_node.next=prev_node
+
+            prev_node=curr_node
+            curr_node=next_node
+
+        self.head=prev_node
